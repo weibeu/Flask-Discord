4d6ddb988364378d7a5fca38fb556419d46a9f3e
Dori Lahav Waisberg <dori.lahav@gmail.com>
2020-02-07 23:26:24 +0200

    Change id to not cast to int and fail if id is not included on UserConnection constructor

--- flask_discord/models/connections.py
+++ flask_discord/models/connections.py
@@ -80,7 +80,7 @@ class UserConnection(object):
 
     def __init__(self, payload):
         self._payload = payload
-        self.id = int(self._payload.get("id", 0))
+        self.id = self._payload["id"]
         self.name = self._payload.get("name")
         self.type = self._payload.get("type")
         self.revoked = self._payload.get("revoked")
